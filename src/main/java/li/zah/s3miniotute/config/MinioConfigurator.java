1e1ff47e85af341d56f5b2b014e2f4041f3fe0a2
David Meggyesy <david.meggyesy@unimelb.edu.au>
2023-05-11 16:31:31 +1000

    * Added Object Storage Service

--- src/main/java/li/zah/s3miniotute/config/MinioConfigurator.java
+++ src/main/java/li/zah/s3miniotute/config/MinioConfigurator.java
@@ -1,18 +1,28 @@
 package li.zah.s3miniotute.config;
 
 import io.minio.MinioClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class MinioConfigurator {
 
+  @Value("${minio.endpoint}")
+  private String endpoint;
+
+  @Value("${minio.key.access}")
+  private String accessKey;
+
+  @Value("${minio.key.secret}")
+  private String secretKey;
+
   private MinioClient minioClient;
 
   @Bean
   public MinioClient minioClient() {
     if (minioClient == null) {
-      minioClient = MinioClient.builder().build();
+      minioClient = MinioClient.builder().endpoint(endpoint).credentials(accessKey, secretKey).build();
     }
     return minioClient;
   }
--- src/test/java/li/zah/s3miniotute/ObjectStorageServiceIT.java
+++ src/test/java/li/zah/s3miniotute/ObjectStorageServiceIT.java
@@ -1,35 +1,29 @@
-package li.zah.s3miniotute.service;
+package li.zah.s3miniotute;
 
-import io.minio.MinioClient;
 import io.minio.errors.ErrorResponseException;
 import io.minio.errors.InsufficientDataException;
 import io.minio.errors.InternalException;
 import io.minio.errors.InvalidResponseException;
 import io.minio.errors.ServerException;
 import io.minio.errors.XmlParserException;
-import io.minio.messages.Bucket;
 import java.io.IOException;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
-import lombok.NonNull;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
+import li.zah.s3miniotute.service.ObjectStorageService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
-@Service
-@RequiredArgsConstructor
-public class ObjectStorageService {
+@SpringBootTest
+public class ObjectStorageServiceIT {
 
-  @NonNull
-  private MinioClient minioClient;
+  @Autowired
+  private ObjectStorageService objectStorageService;
 
-  public void listBuckets()
+  @Test
+  public void objectStorageServiceTest()
       throws ServerException, InsufficientDataException, ErrorResponseException, IOException, NoSuchAlgorithmException, InvalidKeyException, InvalidResponseException, XmlParserException, InternalException {
-
-    for (Bucket bucket : minioClient.listBuckets()) {
-
-      System.out.println(bucket.name() + " - " + bucket.creationDate());
-
-    }
+    objectStorageService.listBuckets();
   }
 
 }
